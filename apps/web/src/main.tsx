27f454e0ffbe369b43d973382ef47886cf13cc7e
loopy-lim <cotmd6203@naver.com>
2025-07-29 16:05:02 +0900

    feat: tanstack router

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { routeTree } from "@/src/routeTree.gen.ts";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import "@workspace/ui/globals.css";
-import App from "./App.tsx";
+
+const router = createRouter({ routeTree });
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,8 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  plugins: [
+    react(),
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    tsconfigPaths(),
+    tailwindcss(),
+  ],
 });
